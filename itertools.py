83e8400cac33200c5d3e2128aef800816fc1ed78
amanneox <amanadhikari2@gmail.com>
2018-05-17 22:36:59 +0530

    itertools-chain

--- itertools.py
+++ itertools.py
@@ -4,3 +4,8 @@ import operator
 months=[x for x in range(10,20) if x%3 or x%4]
 print(months)
 print(list(itertools.accumulate(months,operator.add)))
+
+
+a=range(5)
+b=range(10)
+print(list(itertools.chain(a,b)))
