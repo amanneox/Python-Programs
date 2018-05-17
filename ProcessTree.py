0c44da4ad758150748dc8e6e49d7711e974a488b
amanneox <amanadhikari2@gmail.com>
2018-05-17 22:30:31 +0530

    functools

--- ProcessTree.py
+++ ProcessTree.py
@@ -7,3 +7,10 @@ taxonomy = tree()
 reptilia = taxonomy['Chordata']['Vertebrata']['Reptilia']
 reptilia['Squamata']['Serpentes']['Pythonidae'] = ['Liasis', 'Morelia', 'Python']
 print(json.dumps(taxonomy, indent=4))
+
+path='Chordata.Vertebrata.Reptilia.Squamata.Serpentes'
+path=path.split('.')
+
+family = functools.reduce(lambda a, b: a[b], path, taxonomy)
+family.items()
+dict_items([('Pythonidae', ['Liasis', 'Morelia', 'Python'])])
