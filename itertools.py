65e126fe872efeef541f1f02f4b8a9f1165dff57
amanneox <amanadhikari2@gmail.com>
2018-05-18 10:58:48 +0530

    itertools-take/drop

--- itertools.py
+++ itertools.py
@@ -11,3 +11,7 @@ b=range(10)
 print(list(itertools.chain(a,b)))
 
 print(list(itertools.combinations(range(3),2)))
+
+print(list(itertools.dropwhile(lambda x:x<=3,[1,2,3,4,5])))
+
+print(list(itertools.takewhile(lambda x:x<5,[1,2,3,4,5])))
